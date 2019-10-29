ba4c179830b397522de8a656fdba73426e38703d
Gustavo Paterno <paternogbc@gmail.com>
2019-10-29 16:28:22 -0300

    ajust figure

--- R/01_Fig_linear_regression_freq_size.R
+++ R/01_Fig_linear_regression_freq_size.R
@@ -16,9 +16,9 @@ str(dat)
 # 1. Plot Linear regression-----
 # Freq ~ size
 p1 <- ggplot(dat, aes(y = freq_log, x = size_log)) +
-  geom_point(shape = 1, size = 3) +
+  geom_point(shape = 16, size = 3, color = "steelblue") +
   geom_smooth(method = lm, fill = "gray", color = "black") +
-  theme_classic(base_size = 18) +
+  theme_classic(base_size = 24) +
   labs(y = "Dominant frequency (herzt) [log]",
        x = "Log snout−vent length (mm) [log]")
 p1
@@ -26,5 +26,5 @@ p1
 # 2. Save plot-----
 ggsave(plot = p1, 
        filename = "output/figures/Fig_01_linear_regression_freq_size.png",
-       width = 8, height = 8)
+       width = 8, height = 7)
 # END----
