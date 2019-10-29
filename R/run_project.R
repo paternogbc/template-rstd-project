48328f98cda0db122d4940faa435d433658b5820
Gustavo Paterno <paternogbc@gmail.com>
2019-10-29 16:31:51 -0300

    updated project

--- R/run_project.R
+++ R/run_project.R
@@ -6,8 +6,11 @@
 # 1.1 Load and clean data----
 source(file = "R/01_C_load_clean_anura_data.R")
 
-# 1.2 Fit models----
+# 1.2 Run exploratory analysis and supp figures---------------------------------
+source(file = "R/01_report_exploratory_analysis_study_variables.R")
+
+# 1.3 Fit models----------------------------------------------------------------
 source(file = "R/01_D_fit_linear_regression_freq_size.R")
 
-# 1.3 Plot figures----
+# 1.3 Plot figures--------------------------------------------------------------
 source(file = "R/01_Fig_linear_regression_freq_size.R")
